658b1280669303a052d131b162def831d662fea6
Ryan Bradley <bradleyrp@gmail.com>
2017-07-26 16:04:02 -0400

    removed deprecated method

--- hydrogen_bonding.py
+++ hydrogen_bonding.py
@@ -29,8 +29,8 @@ def hydrogen_bonding(grofile,trajfile,**kwargs):
 	protein_itp_loader = work.vars.get('protein_itp_loader',None)
 	#---default ITP paths are set for each simulation in the metadata
 	if protein_itp_loader==None: 
-		def protein_itp_loader(sn):
-			itp = work.meta.get('sn',{}).get('protein_itp',None)
+		def protein_itp_loader(sn,**kwargs):
+			itp = work.meta.get(sn,{}).get('protein_itp',None)
 			if not itp: raise Exception('cannot find protein_itp in meta dictionary for %s '%sn+
 				'note that you can also use the protein_itp_loader functionality to get the ITP file')
 	#---custom ITP loader specified as an alternate_module
@@ -83,7 +83,7 @@ def hydrogen_bonding(grofile,trajfile,**kwargs):
 		itp_fn = work.meta[sn].get('protein_itp',protein_itp_loader(sn,work=work))
 		if not itp_fn: raise Exception('add protein_itp to the meta for %s'%sn)
 		#---get the sims spot path systematically
-		if os.path.isfile(itp_fn): itp_fn = itp_fn_abs
+		if os.path.isfile(itp_fn): itp_fn_abs = itp_fn
 		#---if path is relative then we consult the spots
 		else:
 			rootdir = work.raw.spots[(work.raw.spotname_lookup(sn),'structure')]['rootdir']
@@ -114,155 +114,6 @@ def hydrogen_bonding(grofile,trajfile,**kwargs):
 				hydrogen_bond_ref[(molname,resname_focus)] = {
 					'acceptors':acceptor_names,'donors':donor_list}
 
-	#---deprecated method below
-	if False:
-
-		#---assemble a collection of ITPs for molecules involved in the hydrogen bonding
-		targets_itps = dict([(resname,land.itps[land.objects[resname]['fn']][resname]) 
-			for resname in targets])
-		#---include any proteins as participants in the bonding
-		if kwargs['calc']['specs'].get('protein',False):
-			#---get the protein ITP from metadata
-			itp_fn = work.meta[sn].get('protein_itp')
-			if not itp_fn: raise Exception('add protein_itp to the meta for %s'%sn)
-			#---get the sims spot path systematically
-			rootdir = work.raw.spots[(work.raw.spotname_lookup(sn),'structure')]['rootdir']
-			sn_dir = os.path.join(rootdir,sn)
-			#---user supplies step folder and path to the reference structure
-			itp_fn_abs = os.path.join(sn_dir,itp_fn)
-			protein_itp = mod['GMXTopology'](itp_fn_abs)
-			#---loop over molecules in the protein ITP
-			for molname,molecule in protein_itp.molecules.items():
-				#---since we are dealing with a protein ITP we chop it up into residues
-				resnrs = np.array([i['resnr'] for i in molecule['atoms']])
-				resnr_changes = resnrs[1:]!=resnrs[:-1]
-				resnr_changes = np.concatenate(([0],np.where(resnr_changes)[0]+1,[len(resnrs)]))
-				res_inds = [np.arange(resnr_changes[i],resnr_changes[i+1]) 
-					for i in range(len(resnr_changes)-2)]
-				#---loop over observed residues
-				for seq in res_inds:
-					resname = list(set([molecule['atoms'][ii]['resname'] for ii in seq]))
-					if len(resname)!=1: 
-						import ipdb;ipdb.set_trace()
-						raise Exception('nonunique resname for this residue in %s'%molname)
-					else: resname = resname[0]
-					mol = dict(atoms=[molecule['atoms'][ii] for ii in seq])
-					#---the next section uses the bonds list to find hydrogens so we attach them here
-					#---! hacked this quickly so worth checking
-					mol['bonds'] = [iii for ii,iii in enumerate(molecule['bonds']) 
-						if any([iii[l] in [jjj['id'] for jj,jjj in enumerate(mol['atoms'])] for l in 'ij'])]
-					#---! could check here to make sure that every residue is exactly the same if you wanted
-					targets_itps[resname] = mol
-		#---for each residue name we collect candidates based on the names
-		for molname,mol in targets_itps.items():
-			#---collect all possible hydrogen bond acceptors
-			acceptor_names = [i['atom'] for i in mol['atoms'] if re.match('^(N|O|S)',i['atom'])]
-			h_name = [i['atom'] for i in mol['atoms'] if re.match('^H',i['atom'])]	
-			try: donor_candidates = [(j,k) for j,k in [(int(i['i']),int(i['j'])) 
-				for i in mol['bonds']] 
-				if any([re.match('^H',mol['atoms'][l-1]['atom']) for l in [j,k]]) 
-				and any([mol['atoms'][m-1]['atom'] in acceptor_names for m in [j,k]])]
-			except:
-				import ipdb;ipdb.set_trace()
-			donor_names = []
-			for d in [(mol['atoms'][i-1]['atom'],mol['atoms'][j-1]['atom']) for i,j in donor_candidates]:
-				if d not in donor_names: donor_names.append(d)
-			#---we store donors and acceptors by residue name. see "note on filtering the bonds" below for 
-			#---...a description of why this might be a problem. this note could be used to 
-			#---...later make this more precise by checking atoms/resnames
-			hydrogen_bond_ref[resname] = {'acceptors':acceptor_names,'donors':donor_names}
-		#---water-naming is hard-coded 
-		hydrogen_bond_ref['water'] = {'donors':[('OW','HW1'),('OW','HW2')],'acceptors':['OW']}
-
-		#---CHECKPOINT: hydrogen_bond_ref has the names for acceptors and donors at this stage
-		#---hydrogen_bond_ref is currently indexed by residue name. this is not necessary for the 
-		#---...filtering step which per the note below is only really operating on names
-
-		#---assemble the names. the final selections are done by atom name only
-		#---note on filtering the bondsit a problem to only use a name? for example, even before adding the 
-		#---...proteins, we are picking up some proteins because they have e.g. an "N" however the atom names 
-		#---...are actually relative to the molecule definition and may be effectively meaningless in some 
-		#---...cases. the alternate take is that many programs like VMD and MDAnalysis also perform 
-		#---...selections this way and as long as we don'tmiss anything then we are fine. we can always catch 
-		#---...errors in the bonds we do find.
-		donors_names = sorted(list(set([m for n in [zip(*i['donors'])[0] 
-			for i in hydrogen_bond_ref.values() if i['donors']!=[]] for m in n])))
-		hydrogens_names = sorted(list(set([m for n in [zip(*i['donors'])[1] 
-			for i in hydrogen_bond_ref.values() if i['donors']!=[]] for m in n])))
-		acceptors_names = sorted(list(set([m for n in [i['acceptors'] 
-			for i in hydrogen_bond_ref.values() if i!=[]]for m in n])))
-
-		#---generate atom groups
-		donors = uni.select_atoms(' or '.join(['name %s'%i for i in donors_names]))
-		acceptors = uni.select_atoms(' or '.join(['name %s'%i for i in acceptors_names]))
-		hydrogens = uni.select_atoms(' or '.join(['name %s'%i for i in hydrogens_names]))#+' or name NA')
-
-		donors_h_pairs = [m for n in [i.get('donors',[]) for i in hydrogen_bond_ref.values()] for m in n]
-		donors_h_pairs_flat = list(set([i for j in donors_h_pairs for i in j]))
-		sel_d,sel_h = [uni.select_atoms(' or '.join(['name %s'%i 
-			for i in list(set(zip(*donors_h_pairs)[j]))])) for j in range(2)]
-		resids = np.unique(np.concatenate([sel_d.resids,sel_h.resids]))
-		donors_side = uni.select_atoms(' or '.join(['name %s'%i for i in donors_h_pairs_flat]))
-		donors_resids = np.unique(donors_side.resids)
-
-		#---identifying residues with both a donor and a corresponding hydrogen
-		both = np.zeros((len(resids),len(donors_h_pairs),2),dtype=bool)
-		alles = np.array([[donors_side.names==i for i in zip(*donors_h_pairs)[j]] for j in range(2)])
-		#---lookups for different atom names (fast because there are relatively few atom names)
-		subsels = [[np.where(i)[0] for i in alles[j]] for j in range(2)]
-		
-		#---loop over heavy/light types
-		for anum in range(2):
-			#---loop over possible pairs
-			for pnum in range(len(alles[anum])):
-				#---crucial conversion back to zero-numbering from resids here
-				try: both[donors_side.resids[subsels[anum][pnum]]-1,pnum,anum] = True
-				except:
-					import ipdb;ipdb.set_trace()
-
-		#---use all to find out which residues have which opportunities for bonding
-		bond_opps = np.transpose(np.where(np.all(both,axis=2)))
-
-		#---some hydrogen bonds have the same donors for multiple hydrogens
-		donors_inds = np.zeros((2,len(bond_opps))).astype(int)
-		for anum in range(2):
-			donors_names_u = np.unique(zip(*donors_h_pairs)[anum])
-			#---for each bond opportunity, we list the heavy donor
-			donors_side_names = np.array(donors_h_pairs).T[anum][bond_opps[:,1]]
-			#---convert this into index (this is fast because it is over a short list of donor names)
-			donors_side_inds = -1*np.ones(len(donors_side_names)).astype(int)
-			for nn,n in enumerate(donors_names_u): donors_side_inds[np.where(donors_side_names==n)] = nn
-			#---lookup from residue and unique heavy donor atom to absolute index in donors_side
-			lookup = len(donors_names_u)*np.ones((len(donors_resids),len(donors_names_u)+1)).astype(int)
-			#---convert this into index (this is fast because it is over a short list of donor names)		
-			donors_side_names_inds = len(donors_names_u)*np.ones((len(donors_side.names))).astype(int)
-			for nn,n in enumerate(donors_names_u): 
-				donors_side_names_inds[np.where(donors_side.names==n)] = nn		
-			lookup[tuple(np.transpose([donors_side.resids-1,donors_side_names_inds]).T)] = \
-				np.arange(len(donors_side.resids))
-			#---translate bond_opps from pair numbering to heavy donor numbering (which is unique)
-			bond_opps_unique = np.array(bond_opps)
-			bond_opps_unique[:,1] = donors_side_inds
-			donors_inds[anum] = lookup[tuple(bond_opps_unique.T)]
-
-		#---prepare the acceptors selections
-		acceptors_names = np.unique([j for k in 
-			[i.get('acceptors',[]) for i in hydrogen_bond_ref.values()] for j in k])
-		acceptors_side = uni.select_atoms(' or '.join(['name %s'%i for i in acceptors_names]))
-
-		#---prepare coordinates for each frame
-		st = time.time()
-		vecs,all_donor_coords,all_acceptor_coords,all_h_coords = [],[],[],[]
-		#---purposefully profligate with the memory so this goes quickly
-		for fr in range(nframes):
-			status('caching coordinates',tag='compute',i=fr,looplen=nframes,start=st)	
-			uni.trajectory[fr]
-			vecs.append(uni.dimensions[:3]/lenscale)
-			all_donor_coords.append(donors_side.positions[donors_inds[0]]/lenscale)
-			all_h_coords.append(donors_side.positions[donors_inds[1]]/lenscale)
-			all_acceptor_coords.append(acceptors_side.positions/lenscale)
-		status('completed caching in %.1f minutes'%((time.time()-st)/60.),tag='status')
-
 	"""
 	developing a new method for selecting the atoms correctly
 	we need to get all possible donors into a big selection after which case the hbonds.hbonder_framewise
@@ -278,6 +129,7 @@ def hydrogen_bonding(grofile,trajfile,**kwargs):
 		for each hydrogen find the associated heavy atom and and add both indices to separate lists
 		net result is two lists of indices over the hydrogens and donors which constitute a bond
 	"""
+
 	#---get the heavy atom side of the donors
 	donors_heavy,donors_h = [uni.select_atoms(' or '.join(['(resname %s and (%s))'%(
 		resname if type(resname) in str_types else resname[1],' or '.join(['name %s'%i[w] 
@@ -364,42 +216,21 @@ def hydrogen_bonding(grofile,trajfile,**kwargs):
 	#---get valid frames
 	valid_frames = np.where([len(i['donors'])>0 for i in incoming])[0]
 	#---concatenate the donor/acceptor indices across all frames
-	donor_cat,donor_cat_h = [np.concatenate([donors_inds[j][incoming[i]['donors']] 
+	donor_cat,donor_cat_h = [np.concatenate([donors_reindex.T[j][incoming[i]['donors']] 
 		for i in valid_frames]).astype(int) for j in range(2)]
 	acceptor_cat = np.concatenate([incoming[i]['acceptors'] for i in valid_frames]).astype(int)
 	obs_by_frames = np.array([len(incoming[i]['acceptors']) for i in valid_frames]).astype(int)
-
-	#---compile the massive bond table
-	if False:
-		tabulation = []
-		for ff,fr in enumerate(valid_frames):
-			status('compiling large bond table',i=ff,looplen=len(valid_frames),tag='compute')
-			iii = incoming[fr]
-			donors_resnames = donors_side.resnames[donors_inds[0][iii['donors']]]
-			donors_resids = donors_side.resids[donors_inds[0][iii['donors']]]
-			donors_names1 = donors_side.names[donors_inds[0][iii['donors']]]
-			donors_names2 = donors_side.names[donors_inds[1][iii['donors']]]
-			acceptors_resnames = acceptors_side.resnames[iii['acceptors']]
-			acceptors_resids = acceptors_side.resids[iii['acceptors']]
-			acceptors_names = acceptors_side.names[iii['acceptors']]
-			tabulation.append([
-				donors_resnames,donors_resids,donors_names1,donors_names2,
-				acceptors_resnames,acceptors_resids,acceptors_names])
-
-	#---aliases for the new method
-	donors_side = donors_heavy
-	acceptors_side = acceptors_heavy
 
 	start_time = time.time()
 	#---tabulate each bond observation
 	status('sluggish sequence because there are {:,} bond observations'.format(len(donor_cat)),tag='warning')
 	status('tabulating all distinct hydrogen bonds',tag='compute')
-	tabulation = np.transpose((donors_side.resnames[donor_cat],donors_side.resids[donor_cat],
-		donors_side.names[donor_cat],acceptors_side.resnames[acceptor_cat],
-		acceptors_side.resids[acceptor_cat],acceptors_side.names[acceptor_cat],
+	tabulation = np.transpose((donors_heavy.resnames[donor_cat],donors_heavy.resids[donor_cat],
+		donors_heavy.names[donor_cat],acceptors_heavy.resnames[acceptor_cat],
+		acceptors_heavy.resids[acceptor_cat],acceptors_heavy.names[acceptor_cat],
 		#---include the hydrogen identity here in the tabulation (note this might make things larger?)
 		#---also note that the hydrogen atom name should be enough because we already have the donor resid
-		donors_side.names[donor_cat_h],))
+		donors_h.names[donor_cat_h],))
 	status('stopwatch: %.1fs'%(time.time()-start_time),tag='compute')
 
 	#---reduce tabulation by discarding all SOL-SOL bonds
