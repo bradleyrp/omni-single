216479fb1f7fbeb276899062dc03aca6cef9f9c4
Ryan Bradley <bradleyrp@gmail.com>
2017-08-22 13:09:07 -0400

    improved box vectors when plotting hulls

--- render/wavevids.py
+++ render/wavevids.py
@@ -79,7 +79,7 @@ def print_birdseye_snapshot_render(surfs,protpts,mvecs,nprots,handle='',
 	else: plt.show()
 
 def plothull(ax,points,griddims=None,vecs=None,c=None,mset=None,
-	alpha=None,nine=False,fill=None,radius=0.5,ec=None,lw=1):
+	alpha=None,nine=False,fill=None,radius=0.5,ec=None,lw=1,zorder=1):
 	"""
 	Plot an outline around each protein.
 	This function was copied from elsewhere.
@@ -88,7 +88,8 @@ def plothull(ax,points,griddims=None,vecs=None,c=None,mset=None,
 	if alpha == None: alpha = 0.65
 	if fill == None: fill = True
 	if type(vecs)==type(None): vecs = griddims
-	m,n = griddims
+	if not griddims: m,n = 1.0,1.0
+	else: m,n = griddims
 	pts = np.array([[i[0]*m/vecs[0],i[1]*n/vecs[1]] for i in points])
 	patches = []
 	#---you must send a vector with three dimensions: protein index, atom, xy
@@ -96,6 +97,6 @@ def plothull(ax,points,griddims=None,vecs=None,c=None,mset=None,
 		hull = scipy.spatial.ConvexHull(pts)
 		p = ax.add_patch(mpl.patches.Polygon(pts[hull.vertices],
 			closed=True,fill=(True if fill else False),
-			facecolor=c,lw=lw,alpha=alpha,edgecolor=(c if ec == None else ec)))
+			facecolor=c,lw=lw,alpha=alpha,edgecolor=(c if ec == None else ec),zorder=zorder))
 		patches.append(p)
 	return patches
