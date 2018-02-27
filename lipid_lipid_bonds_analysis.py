0683aaa956a219f10a4e891c54153dbf2853e0db
Ryan Bradley <bradleyrp@gmail.com>
2018-02-27 11:43:33 -0500

    adapting lipid_lipid_bonds_analysis for ptdins

--- lipid_lipid_bonds_analysis.py
+++ lipid_lipid_bonds_analysis.py
@@ -14,8 +14,29 @@ def load_actinlink(data):
 	if set(sns)!=set(work.sns()): raise Exception
 	nmol_counts = dict([(sn,dict(zip(*[data['hydrogen_bonding'][sn]['data'][k] 
 		for k in ['resnames','nmols']]))) for sn in sns])
+	residue_renamer = None
 	outgoing = dict(sns=sns,color_by_simulation=color_by_simulation,
-		replicate_mapping=replicate_mapping,extra_labels=extra_labels,nmol_counts=nmol_counts)
+		replicate_mapping=replicate_mapping,extra_labels=extra_labels,nmol_counts=nmol_counts,
+		residue_renamer=residue_renamer)
+	return outgoing
+
+def load_ptdins(data):
+	collection = 'asymmetric_all'
+	sns = work.metadata.collections[collection]
+	if set(sns)!=set(work.sns()): raise Exception('plot is set up for %s'%collection)
+	def color_by_simulation(sn):
+		return colorize(work.meta[sn],comparison='asymmetric_all')
+	replicate_mapping = dict([(sn,[sn]) for sn in sns])
+	extra_labels = dict([(sn,'%s, %s'%(work.meta[sn]['ptdins_label'],work.meta[sn]['ion_label'])) 
+		for sn in sns])
+	nmol_counts = dict([(sn,dict(zip(*[data['hydrogen_bonding'][sn]['data'][k] 
+		for k in ['resnames','nmols']]))) for sn in sns])
+	def residue_renamer(resname):
+		if resname in ['PI2P','P35P','PIPP','PIPU','SAPI']: return 'PtdIns'
+		else: return resname
+	outgoing = dict(sns=sns,color_by_simulation=color_by_simulation,
+		replicate_mapping=replicate_mapping,extra_labels=extra_labels,nmol_counts=nmol_counts,
+		residue_renamer=residue_renamer)
 	return outgoing
 
 @autoload(plotrun)
@@ -50,6 +71,7 @@ def plot_bonds(name,kinds,**kwargs):
 	"""Plot the distributions of observed bond counts."""
 	merged = kwargs.get('merged',False)
 	normed = kwargs.get('normed',False)
+	symmetrize = kwargs.get('symmetrize',False)
 	# fancy imshow method is a great substitute for error bars
 	style = kwargs.get('style',['bars','imshow'][-1])
 	#! hardcoded settings
@@ -78,6 +100,7 @@ def plot_bonds(name,kinds,**kwargs):
 	else: replicate_mapping_this = [(sn,[sn]) for sn in sns]
 	# tabulate nframes by simulation
 	nframes_by_sn = dict([(sn,len(data['salt_bridges'][sn]['data']['observations'])) for sn in sns])
+	valid_frames_by_kind = dict([(sn,dict([(k,data[k][sn]['data']['valid_frames']) for k in kinds])) for sn in sns])
 	# tabulate the bonds
 	for sn_group,sns_this in replicate_mapping_this:
 		for snum,sn in enumerate(sns_this):
@@ -91,18 +114,21 @@ def plot_bonds(name,kinds,**kwargs):
 				resnames = [i for i in np.unique(np.concatenate((subjects,targets)))
 					if i not in resnames_exclude]
 				combos = list(itertools.product(resnames,repeat=2))
-				for combo in combos:
+				if residue_renamer!=None: 
+					combos_renamed = [tuple([residue_renamer(i) for i in j]) for j in combos]
+				else: combos_renamed = combos
+				for combo,combo_renamed in zip(combos,combos_renamed):
 					rows = np.where(np.all((
 						bonds[:,rowspec.index('subject_resname')]==combo[0],
 						bonds[:,rowspec.index('target_resname')]==combo[1],
 						),axis=0))[0]
 					if len(rows)>0:
 						if sn_group not in post: post[sn_group] = {}
-						if combo not in post[sn_group]: 
+						if combo_renamed not in post[sn_group]: 
 							#! we have to load with zeros here because one simulation has a single CHL1-CHL1
 							#! ... bond while the other replicate lacks it, and later we are summing and 
 							#! ... concatenating hence we need proper placeholders
-							post[sn_group][combo] = [[np.zeros(nframes_by_sn[ii]) 
+							post[sn_group][combo_renamed] = [[np.zeros(nframes_by_sn[ii]) 
 								for ii in sns_this] for jj in kinds]
 						counts = obs[:,rows].sum(axis=1)
 						# we have to be careful about counting. we want to concatenate simulations
@@ -110,21 +136,55 @@ def plot_bonds(name,kinds,**kwargs):
 						# ... list here, and then process it when we are done. the first dimension gets the 
 						# ... while the second dimension is contatenated 
 						#! post[sn_group][combo] = np.append(post[sn_group][combo],counts)
-						post[sn_group][combo][knum][snum] = counts
+						post[sn_group][combo_renamed][knum][snum] = counts
 	# post-processing to sum bond types (first dimension) then concatenate replicates
 	for sn_group in post:
 		for combo in post[sn_group]:
 			try:
 				post[sn_group][combo] = np.concatenate(np.array(post[sn_group][combo]).sum(axis=0))
-			except: raise Exception('failed to process %s,%s'%(sn_group,combo))
+			# quarantining the hacking for PtdIns
+			except: 
+				#! dark is stuck on scipy 0.19.1 while the actinlink machine has 1.0.0 where they fixed the
+				#! ... bug with kdtree where you needed two copies of the box vectors and some frames broke
+				#! ... however rather than recompute with new scipy I will just use the standard fix for when
+				#! ... there is a mismatch in the frames: valid_frames tells us which ones work
+				if len(dict(replicate_mapping_this)[sn_group])>1: 
+					raise Exception('the valid_frames fix does not work for replicates')
+				intersect = np.intersect1d(*[data[k][sn_group]['data']['valid_frames'] for k in kinds])
+				valid_frame_map = [np.array([np.where(np.arange(nframes_by_sn[sn_group])==i)[0][0] 
+					for i in intersect]) for k in kinds]
+				#! running out of names. hacking through this swiftly!
+				valid_frame_remap = [np.array([np.where(i==v)[0][0] 
+					for i in intersect]) for v in valid_frame_map]
+				try:
+					post[sn_group][combo] = [[post[sn_group][combo][kk][ss][valid_frame_remap[kk]] 
+						for ss,s in enumerate(dict(replicate_mapping_this)[sn_group])] 
+						for kk,kind in enumerate(kinds)]
+				# some bond types have an empty bond list hence an empty array so we hack through this
+				# ... by inferring the length of the trajectory and using zeros when the array is empty. 
+				# ... note that this only happens when you drop the salt bridge cutoff down from 3.4 to 2,2
+				except:
+					post[sn_group][combo] = [[i if len(i)>0 else [np.zeros(max([len(k[0]) 
+						for k in post[sn_group][combo]]))] for i in j] for j in post[sn_group][combo]]					
+				post[sn_group][combo] = np.concatenate(np.array(post[sn_group][combo]).sum(axis=0))
+	if symmetrize:
+		post_symmetric = {}
+		for sn in post:
+			post_symmetric[sn] = {}
+			combos_sym = list(set([tuple(sorted(i)) for i in post[sn].keys()]))
+			for combo in combos_sym:
+				post_symmetric[sn][combo] = np.sum([post[sn][c] for c in post[sn] if set(c)==set(combo)],axis=0)
+		import ipdb;ipdb.set_trace()
+		post = post_symmetric
 	# reformulate post into images
 	combos_u = list(set([k for j in [i.keys() for i in post.values()] for k in j]))
 	sns_by_mapping = list(zip(*replicate_mapping_this))[0]
-	max_count = int(np.concatenate([np.concatenate(post[sn].values()) for sn in post]).max())
+	try: max_count = int(np.concatenate([np.concatenate(post[sn].values()) for sn in post]).max())
+	except: max_count = int(np.concatenate([np.array(post[sn].values()).reshape(-1) for sn in post]).max())
 	if style=='imshow':
 		images = dict([(combo,{}) for combo in combos_u])
 		for combo in combos_u:
-			sns_this = [sn for sn in sns_by_mapping if combo in post[sn]]
+			sns_this = [sn for sn in sns_by_mapping if sn in post and combo in post[sn]]
 			images[combo] = np.zeros((len(sns_this),max_count))
 			for snum,sn in enumerate(sns_this):
 				if combo not in post[sn]: continue
@@ -136,10 +196,9 @@ def plot_bonds(name,kinds,**kwargs):
 					images[combo][snum] = hist
 		# each image of the lipid-count-normalized histograms has a maximum value which we get from
 		# ... the maximum of each simulation's max bond counts divided by the normalization factor
-		max_count = int(np.concatenate([np.concatenate(post[sn].values()) for sn in post]).max())
 		post_norm,post_norm_interp = {},{}
 		for combo in combos_u:
-			sns_this = [sn for sn in sns_by_mapping if combo in post[sn]]
+			sns_this = [sn for sn in sns_by_mapping if sn in post and combo in post[sn]]
 			obs_by_snum = []
 			post_norm_interp[combo] = {}
 			for sn in sns_this:
@@ -154,7 +213,9 @@ def plot_bonds(name,kinds,**kwargs):
 			post_norm[combo] = obs_by_snum
 			#!np.concatenate(obs_by_snum).max()
 		#! simple image method which may be deprecated by the superior interpolation method
+		#! THIS SECTION IS DEPRECATED. Use the "fancy" method even when not doing normalization
 		if not do_fancy_interp:
+			#! changes at the end of the following are incorrect.
 			# get the global maximum of the normed counts
 			max_norm = max([np.concatenate(i).max() for i in post_norm.values()])
 			bins_global = np.linspace(0,max_norm,nbins_normed)
@@ -164,7 +225,7 @@ def plot_bonds(name,kinds,**kwargs):
 				images[combo] = []
 				norm_max_local = np.concatenate(post_norm[combo]).max()
 				bins_local = np.linspace(0,norm_max_local,nbins_normed)
-				for item in post_norm[combo]:
+				for item,sn in zip(post_norm[combo],sns_this):
 					"""
 					in this standard method we use uniform bins to make a uniform image however this fails
 					to look reasonable because the distributions with the largest normalization factor
@@ -191,18 +252,25 @@ def plot_bonds(name,kinds,**kwargs):
 					hist,_ = np.histogram(item,bins=bins,normed=True)
 					images[combo].append(hist)
 				images[combo] = np.array(images[combo])
+				# reformulate by simulation color (sorry this is messy. written quickly but works)
+				images[combo] = (np.concatenate((np.tile(np.ones(images[combo].shape),(3,1,1)),
+					[images[combo]]))*np.tile(np.array(mpl.colors.to_rgba(color_by_simulation(sn))),
+					(images[combo].shape[0],images[combo].shape[1],1)).transpose((2,0,1))).transpose((2,1,0))
 		# loop over normed data and interpolate. note that iterpolation is *essential* to rendering
 		# ... this data correctly because we have integer counts normalized by floats (see note above)
 		else:
 			images = {}
 			for combo in combos_u:
-				sns_this = [sn for sn in sns_by_mapping if combo in post[sn]]
-				base_res = 1000
-				# we construct a high resolution image and just round things onto it
-				image = np.zeros((base_res,base_res,4))
+				sns_this = [sn for sn in sns_by_mapping if sn in post and combo in post[sn]]
 				# the number of bins is the maximum of the discrete count of bins. we could modify this later to 
 				# ... exclude some whitespace if necessary
 				nbins = float(max_count)
+				# if we are norming we use a standard resolution and then round into the bins
+				if normed: base_res = 1000
+				# if we are not norming we keep absolute counts
+				else: base_res = int(nbins)
+				# we construct a high resolution image and just round things onto it
+				image = np.zeros((base_res,base_res,4))
 				shrink_max = float(min([v['norm_factor'] for k,v in post_norm_interp[combo].items()]))
 				# after normalization, the top of the bar is the maximum count by the maximum shrink
 				max_factor = nbins/shrink_max
@@ -245,12 +313,15 @@ def plot_bonds(name,kinds,**kwargs):
 						lrbt = np.array([xpos,xpos+width_bins,
 							bin_num*height_bins,(bin_num+1)*height_bins])
 						inds = np.round(lrbt*base_res).astype(int)
+						# if we multiply the rgba value by intensity then it fades to grey
+						# ... so instead we multiply only the alpha by intensity. this fades to background
+						# ... and there is nothing behind it so no overlays. this looks better.
 						image[inds[0]:inds[1],inds[2]:inds[3]] = np.array(mpl.colors.to_rgba(
-							color_by_simulation(sn)))*intensity
+							color_by_simulation(sn)))*np.array([1.,1.,1.,intensity])
 					xpos += width_bins
 				# transpose so the image looks correct on imshow using the 3-dimensional color values
 				images[combo] = np.transpose(image,(1,0,2))/image.max()
-			max_row_global = max([max(np.where(np.sum(v[:,:,:3].sum(axis=2),axis=1)>0)[0]) for v in images.values()])
+		max_row_global = max([max(np.where(np.sum(v[:,:,:3].sum(axis=2),axis=1)>0)[0]) for v in images.values()])
 	# plot
 	axes,fig = square_tiles(len(combos_u),figsize=16,wspace=wspace,hspace=hspace)
 	for cnum,combo in enumerate(combos_u):
@@ -302,10 +373,13 @@ def plot_bonds(name,kinds,**kwargs):
 		(' (normalized)' if normed else ''),fontsize=20,y=0.92))
 	picturesave('fig.lipid_lipid_bonds.%s.%s'%(style,name),work.plotdir,
 		backup=False,version=True,meta={},extras=extras)
+	post_debugger[tuple(kinds)] = post
 
 def plots_actinlink():
 	"""Plot actinlink bonds analysis including merged replicates."""
 	figspec = {}
+	global post_debugger
+	post_debugger = {}
 	kind_map = {'hbonds':['hydrogen_bonding'],'salt':['salt_bridges'],
 		'hbonds_salt':['hydrogen_bonding','salt_bridges']}
 	for normed in [True,False]:
@@ -313,7 +387,22 @@ def plots_actinlink():
 			for key,kind in kind_map.items():
 				name = key+('.merged' if merged else '')+('.normed' if normed else '')
 				figspec[name] = {'merged':merged,'kinds':kind,'normed':normed}
-	print figspec.keys()
+	#! testing figspec = {'salt.normed':figspec['salt.normed']}
+	for key,spec in figspec.items(): plot_bonds(name=key,**spec)
+
+def plots_ptdins():
+	"""Plot actinlink bonds analysis including merged replicates."""
+	figspec = {}
+	global post_debugger
+	post_debugger = {}
+	kind_map = {'hbonds':['hydrogen_bonding'],'salt':['salt_bridges'],'hbonds_salt':['hydrogen_bonding','salt_bridges']}
+	# no need to norm because the concentrations are identical
+	normed = False
+	merged = False
+	symmetrize = True
+	for key,kind in kind_map.items():
+		name = key+('.normed' if normed else '')
+		figspec[name] = {'merged':merged,'kinds':kind,'normed':normed,'symmetrize':symmetrize}
 	#! testing figspec = {'salt.normed':figspec['salt.normed']}
 	for key,spec in figspec.items(): plot_bonds(name=key,**spec)
 
